5cab4936b673ac6a56b16b78ff778a4142e9174e
kallexm <kallexm@hotmail.com>
2023-04-30 22:21:35 +0200

    apps: hikari_sword: Implement glow mode
    Implement glow mode and set preliminary
    delays between led groups.
    
    None of the glow, wave or sole modes are
    tested.
    
    Signed-off-by: kallexm <kallexm@hotmail.com>

--- applications/hikari_sword/src/modes/glow.c
+++ applications/hikari_sword/src/modes/glow.c
@@ -11,25 +11,28 @@
 #include "slab_event.h"
 #include "../lib/slab/events/slab_event_rgb.h"
 
-/* LED resources */
-static struct light_resource *l1;
-static struct light_resource *l2;
-static struct light_resource *l3;
-static struct light_resource *l4;
+#include "default_resources.h"
 
 /* Slabs */
 static struct slab *st;
 static struct slab *sg;
 static struct slab *sc;
+
+static struct slab *cb1;
+
 static struct slab *sd1;
 static struct slab *sd2;
 static struct slab *sd3;
-static struct slab *sl1;
-static struct slab *sl2;
-static struct slab *sl3;
-static struct slab *sl4;
-
-static struct slab *cb1;
+static struct slab *sd4;
+static struct slab *sd5;
+static struct slab *sd6;
+static struct slab *sd7;
+static struct slab *sd8;
+static struct slab *sd9;
+static struct slab *sd10;
+static struct slab *sd11;
+static struct slab *sd12;
+static struct slab *sd13;
 
 static void print_callback(struct slab_event *evt, void *ctx)
 {
@@ -44,73 +47,171 @@ static void glow_constructor(void)
 {
 	light_res_err_t res_err = 0;
 
-	res_err = light_resource_use("Lguard_1", &l1)
-			  | light_resource_use("Lguard_2", &l2)
-			  | light_resource_use("Lguard_3", &l3)
-			  | light_resource_use("Lguard_4", &l4);
+	res_err = USE_ALL_HIKARI_LIGHT_RESOURCES;
 	if (res_err) {
 		printk("resource use err %d", res_err);
 		k_oops();
 	}
 
+	CREATE_ALL_HIKARI_LIGHT_SLABS;
+
+	/* Source Generator */
 	struct slab_glower_config sg_config = {
 		.hue = 130, .sat = 0.9,
 		.val = {.a = 0.25, .b = 0.25, .ym = 0.3, .yd = 4.0}
 	};
-
 	st = slab_create(SLAB_TYPE_TICKER, K_MSEC(25));
 	sg = slab_create(SLAB_TYPE_GLOWER, &sg_config);
 	sc = slab_create(SLAB_TYPE_HSV2RGB);
-	sd1 = slab_create(SLAB_TYPE_DELAY, 20);
-	sd2 = slab_create(SLAB_TYPE_DELAY, 20);
-	sd3 = slab_create(SLAB_TYPE_DELAY, 20);
-	sl1 = slab_create(SLAB_TYPE_LED, l1->data, LED_TYPE_RGB);
-	sl2 = slab_create(SLAB_TYPE_LED, l2->data, LED_TYPE_RGB);
-	sl3 = slab_create(SLAB_TYPE_LED, l3->data, LED_TYPE_RGB);
-	sl4 = slab_create(SLAB_TYPE_LED, l4->data, LED_TYPE_RGB);
-
-	cb1 = slab_create(SLAB_TYPE_NOTIFIER, print_callback, NULL);
-	slab_connect(cb1, sl3);
-
 	slab_connect(sg, st);
 	slab_connect(sc, sg);
 
+	/* Debug callback */
+	cb1 = slab_create(SLAB_TYPE_NOTIFIER, print_callback, NULL);
+	slab_connect(cb1, sc);
+
+	/* Start glow from core */
+	slab_connect(slc[0], sc);
+	slab_connect(slc[1], slc[0]);
+	slab_connect(slc[2], slc[0]);
+	slab_connect(slc[3], slc[0]);
+	slab_connect(slc[4], slc[0]);
+	slab_connect(slc[5], slc[0]);
+
+	/* Pommel glow */
+	sd1 = slab_create(SLAB_TYPE_DELAY, 80);
 	slab_connect(sd1, sc);
-	slab_connect(sl1, sc);
+	slab_connect(slp[0], sd1);
+	slab_connect(slp[1], sd1);
 
-	slab_connect(sd2, sd1);
-	slab_connect(sl2, sd1);
-	
+	/* Inner blade glow stage 1 */
+	sd2 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd2, sc);
+	slab_connect(slsq[0], sd2);
+	slab_connect(slsq[1], sd2);
+
+	/* Inner blade glow stage 2 */
+	sd3 = slab_create(SLAB_TYPE_DELAY, 40);
 	slab_connect(sd3, sd2);
-	slab_connect(sl3, sd2);
+	slab_connect(slms[0], sd3);
+	slab_connect(slms[1], sd3);
+	slab_connect(slms[2], sd3);
+	slab_connect(slms[3], sd3);
+	slab_connect(slms[4], sd3);
+	slab_connect(slms[5], sd3);
 
-	slab_connect(sl4, sd3);
+	/* Inner blade glow stage 3 */
+	sd4 = slab_create(SLAB_TYPE_DELAY, 40);
+	slab_connect(sd4, sd3);
+	slab_connect(slts[0], sd4);
+	slab_connect(slts[1], sd4);
+
+	/* Outer blade glow stage 1 */
+	sd5 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd5, sc);
+	slab_connect(sllb[0], sd5);
+	slab_connect(sllb[1], sd5);
+	slab_connect(slrb[0], sd5);
+	slab_connect(slrb[1], sd5);
+
+	/* Outer blade glow stage 2 */
+	sd6 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd6, sd5);
+	slab_connect(sllb[2], sd6);
+	slab_connect(sllb[3], sd6);
+	slab_connect(slrb[2], sd6);
+	slab_connect(slrb[3], sd6);
+
+	/* Outer blade glow stage 3 */
+	sd7 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd7, sd6);
+	slab_connect(sllb[4], sd7);
+	slab_connect(sllb[5], sd7);
+	slab_connect(slrb[4], sd7);
+	slab_connect(slrb[5], sd7);
+
+	/* Outer blade glow stage 4 */
+	sd8 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd8, sd7);
+	slab_connect(sllt[2], sd8);
+	slab_connect(sllt[3], sd8);
+	slab_connect(sllt[6], sd8);
+	slab_connect(sllt[7], sd8);
+	slab_connect(slrt[2], sd8);
+	slab_connect(slrt[3], sd8);
+	slab_connect(slrt[6], sd8);
+	slab_connect(slrt[7], sd8);
+
+	/* Outer blade glow stage 5 */
+	sd9 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd9, sd8);
+	slab_connect(sllt[0], sd9);
+	slab_connect(sllt[1], sd9);
+	slab_connect(sllt[4], sd9);
+	slab_connect(sllt[5], sd9);
+	slab_connect(slrt[0], sd9);
+	slab_connect(slrt[1], sd9);
+	slab_connect(slrt[4], sd9);
+	slab_connect(slrt[5], sd9);
+
+	/* Spike glow stage 1 */
+	sd10 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd10, sc);
+	slab_connect(slls[0], sd10);
+	slab_connect(slls[1], sd10);
+	slab_connect(slrs[0], sd10);
+	slab_connect(slrs[1], sd10);
+
+	/* Spike glow stage 2 */
+	sd11 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd11, sd10);
+	slab_connect(slls[2], sd11);
+	slab_connect(slrs[2], sd11);
+
+	/* Guard glow stage 1 */
+	sd12 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd12, sc);
+	slab_connect(sllg[0], sd12);
+	slab_connect(sllg[3], sd12);
+	slab_connect(slrg[0], sd12);
+	slab_connect(slrg[3], sd12);
+
+	/* Guard glow stage 2 */
+	sd13 = slab_create(SLAB_TYPE_DELAY, 20);
+	slab_connect(sd13, sc);
+	slab_connect(sllg[1], sd13);
+	slab_connect(sllg[2], sd13);
+	slab_connect(slrg[1], sd13);
+	slab_connect(slrg[2], sd13);
 }
 
 static void glow_destructor(void)
 {
 	light_res_err_t res_err = 0;
 
-	slab_destroy(sl4);
-
-	slab_destroy(sl3);
-	slab_destroy(sd3);
-
-	slab_destroy(sl2);
-	slab_destroy(sd2);
-
-	slab_destroy(sl1);
-	slab_destroy(sd1);
+	DESTROY_ALL_HIKARI_LIGHT_SLABS;
 
 	slab_destroy(sc);
 	slab_destroy(sg);
 	slab_destroy(st);
 
-	res_err = light_resource_return(l1)
-			  | light_resource_return(l2)
-			  | light_resource_return(l3)
-			  | light_resource_return(l4);
+	slab_destroy(cb1);
 
+	slab_destroy(sd1);
+	slab_destroy(sd2);
+	slab_destroy(sd3);
+	slab_destroy(sd4);
+	slab_destroy(sd5);
+	slab_destroy(sd6);
+	slab_destroy(sd7);
+	slab_destroy(sd8);
+	slab_destroy(sd9);
+	slab_destroy(sd10);
+	slab_destroy(sd11);
+	slab_destroy(sd12);
+	slab_destroy(sd13);
+
+	res_err = RETURN_ALL_HIKARI_LIGHT_RESOURCES;
 	if (res_err) {
 		printk("resource return err %d", res_err);
 		k_oops();
@@ -121,29 +222,6 @@ static void glow_destructor(void)
 {
 	slab_stim(st, slab_event_create(SLAB_EVENT_RESET));
 }
-
-/* Tweak function implementation could be dropped.
-   In this case, posting a tweak event will do nothing. */
-
-/*void glow_tweak_color(float hue)
-{
-
-}
-
-void glow_tweak_intensity(float saturation)
-{
-
-}
-
-void glow_tweak_gain(float value)
-{
-
-}
-
-void glow_tweak_speed(float speed)
-{
-
-}*/
 
 static struct hikari_light_mode_api glow_api = {
 	.constructor = glow_constructor,
